d815c40146f1c1c35caa189f7452541e52a8b500
Keith Lashley <lashleykeith@gmail.com>
2020-11-24 20:01:06 +0900

    Add files via upload

---
+++ java/com/example/kotlinuberremake/Common.kt
@@ -0,0 +1,6 @@
+package com.example.kotlinuberremake
+
+object Common {
+
+    val DRIVER_INFO_REFERENCE: String = "DriverInfo"
+}
---
+++ java/com/example/kotlinuberremake/Model/DriverInfoModel.kt
@@ -0,0 +1,8 @@
+package com.example.kotlinuberremake.Model
+
+class DriverInfoModel {
+    var firstName:String=""
+    var lastName:String=""
+    var phoneNumber:String=""
+    var rating=0.0
+}
